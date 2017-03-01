45fcf66c7322e8b9e9d248df54e3ca6e67973c9b
venkata mangina <alfredjoey1@gmail.com>
2017-03-01 13:04:09 -0500

    first commit

---
+++ Consumer/src/test/java/test/org/vensorcollections/jms/listener/ConsumerListenerTest.java
@@ -0,0 +1,32 @@
+package test.org.vensorcollections.jms.listener;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+
+import javax.jms.TextMessage;
+
+import org.apache.activemq.advisory.ConsumerEvent;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.vendorcollection.jms.listener.ConsumerListener;
+public class ConsumerListenerTest {
+
+	private TextMessage textmssg;
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		ConsumerListener consumerlistener = new ConsumerListener();
+		consumerlistener.onMessage(textmssg);  
+		assertNull(textmssg);
+	}
+
+}
