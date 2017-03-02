19f2160bbc678709a5dd82c12a2d9a8bad8818d3
venkata mangina <alfredjoey1@gmail.com>
2017-03-02 17:19:09 -0500

    Producer baseline checkin

---
+++ Producer/src/main/java/org/vendorcollection/producer/model/Vendor.java
@@ -0,0 +1,52 @@
+package org.vendorcollection.producer.model;
+
+public class Vendor {
+private String vendorname;
+private String firstName;
+private String lastName;
+private String city;
+private String zipcode;
+private String phonenumber;
+public String getVendorname() {
+	return vendorname;
+}
+public void setVendorname(String vendorname) {
+	this.vendorname = vendorname;
+}
+public String getFirstName() {
+	return firstName;
+}
+public void setFirstName(String firstName) {
+	this.firstName = firstName;
+}
+public String getLastName() {
+	return lastName;
+}
+public void setLastName(String lastName) {
+	this.lastName = lastName;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public String getZipcode() {
+	return zipcode;
+}
+public void setZipcode(String zipcode) {
+	this.zipcode = zipcode;
+}
+public String getPhonenumber() {
+	return phonenumber;
+}
+public void setPhonenumber(String phonenumber) {
+	this.phonenumber = phonenumber;
+}
+@Override
+public String toString() {
+	return "Vendor [vendorname=" + getVendorname() + ", firstName=" + getFirstName() + ", lastName=" + getLastName() + ", city=" + getCity()
+			+ ", zipcode=" + getZipcode() + ", phonenumber=" + getPhonenumber() + "]";
+}
+
+}
